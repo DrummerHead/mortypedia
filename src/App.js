db32c2767bf5974af86f13beec479916f09ff607
DrummerHead <drummerhead@gmail.com>
2017-04-09 19:14:03 -0300

    Mock selection menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import data from './data/data.json';
+
 import MortyRow from './MortyRow'
+
+import './selectList.css'
+
 
 class App extends React.Component {
   render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>name</th>
-            <th>type</th>
-            <th>total</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.sort((a, b) => b.total - a.total).map(morty =>
-            <MortyRow key={morty.id} {...morty} />
-          )}
-        </tbody>
-      </table>
+      <main>
+        <div className='selectList'>
+          <span className='selectList__option selectList--selected'>All</span>
+          <span className='selectList__option'>Mine</span>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>type</th>
+              <th>total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.sort((a, b) => b.total - a.total).map(morty =>
+              <MortyRow key={morty.id} {...morty} />
+            )}
+          </tbody>
+        </table>
+      </main>
     );
   }
 }
