7b7f315a05dc08fb32311ef7acf15b2c588dae35
DrummerHead <drummerhead@gmail.com>
2017-04-09 21:43:56 -0300

    Switch between all and mine

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
     super();
     this.state = {
       mortyList: userList.getAll(),
+      isAll: true
     }
     this.selectMorty = this.selectMorty.bind(this);
     this.isMine = this.isMine.bind(this);
@@ -36,12 +37,22 @@ class App extends React.Component {
     return this.state.mortyList.includes(id);
   }
 
+  selectAll(isAll) {
+    this.setState({
+      isAll: isAll
+    })
+  }
+
   render() {
+    const mortyList = this.state.isAll
+      ? data
+      : data.filter(morty => this.isMine(morty.id))
+
     return (
       <main>
         <div className='selectList'>
-          <span className='selectList__option selectList--selected'>All</span>
-          <span className='selectList__option'>Mine</span>
+          <span className={`selectList__option ${this.state.isAll ? 'selectList--selected' : ''}`} onClick={() => this.selectAll(true)}>All</span>
+          <span className={`selectList__option ${!this.state.isAll ? 'selectList--selected' : ''}`} onClick={() => this.selectAll(false)}>Mine</span>
         </div>
         <table>
           <thead>
@@ -53,7 +64,7 @@ class App extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {data.sort((a, b) => b.total - a.total).map(morty =>
+            {mortyList.sort((a, b) => b.total - a.total || b.atk - a.atk || b.id - a.id ).map(morty =>
               <MortyRow key={morty.id} onClick={ev => this.selectMorty(ev, morty.id)} {...morty} isMine={this.isMine(morty.id)} />
             )}
           </tbody>
