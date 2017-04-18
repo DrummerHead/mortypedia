d303f882bf2088d7d1aea91d8cb22250b8df3e57
DrummerHead <drummerhead@gmail.com>
2017-04-18 16:52:23 -0300

    Have first morty by default, refactor

--- src/UserList.js
+++ src/UserList.js
@@ -1,36 +1,41 @@
 class UserList {
   constructor() {
-    this.mortyList = [];
+    this.mortyList = [1];
     this.initialize();
   }
 
   initialize() {
     if (localStorage.getItem('mortyList') === null) {
-      localStorage.setItem('mortyList', JSON.stringify([]));
+      localStorage.setItem('mortyList', JSON.stringify(this.mortyList));
     } else {
       this.mortyList = JSON.parse(localStorage.getItem('mortyList'));
     }
+  }
+
+  sync() {
+    localStorage.setItem('mortyList', JSON.stringify(this.mortyList));
+  }
+
+  add(id) {
+    this.mortyList.push(id);
+    this.sync();
+    //this.debug();
+    return this.mortyList;
+  }
+
+  remove(id) {
+    this.mortyList = this.mortyList.filter(prevId => prevId !== id);
+    this.sync();
+    //this.debug();
+    return this.mortyList;
   }
 
   getAll() {
     return this.mortyList;
   }
 
-  add(id) {
-    this.mortyList.push(id);
-    localStorage.setItem('mortyList', JSON.stringify(this.mortyList));
-    //   this.debug();
-    return this.mortyList;
-  }
-
-  remove(id) {
-    this.mortyList = this.mortyList.filter(prevId => prevId !== id);
-    localStorage.setItem('mortyList', JSON.stringify(this.mortyList));
-    //   this.debug();
-    return this.mortyList;
-  }
-
   debug() {
+    console.log('----------------------------')
     console.info('this.mortyList');
     console.log(this.mortyList);
     console.info('JSON.parse(localStorage.getItem("mortyList"))');
